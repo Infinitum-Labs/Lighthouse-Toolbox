98f6f3f61a15ee45de4850f114ee11874f03fbd9
Siddharth Chitikela <103735905+Obsidian418@users.noreply.github.com>
2022-07-04 13:16:14 +0000

    Initial commit

---
+++ .devcontainer/devcontainer_setup.sh
@@ -0,0 +1,13 @@
+# Install Dart
+sudo apt-get update
+sudo apt-get install apt-transport-https
+wget -qO- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo gpg --dearmor -o /usr/share/keyrings/dart.gpg
+echo 'deb [signed-by=/usr/share/keyrings/dart.gpg arch=amd64] https://storage.googleapis.com/download.dartlang.org/linux/debian stable main' | sudo tee /etc/apt/sources.list.d/dart_stable.list
+sudo apt-get update
+sudo apt-get install dart
+
+# Add Dart to path
+echo 'export PATH="$PATH:/usr/lib/dart/bin"' >> ~/.profile
+
+# Command Palette
+# dart: use recommended settings
---
+++ README.md
@@ -0,0 +1,3 @@
+# Lighthouse Toolbox
+
+Lighthouse Toolbox is a command-line application that provides development tools that can be used when working on [Lighthouse](https://github.com/Infinitum-Labs/Lighthouse-Project) code.
